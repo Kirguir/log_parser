23da4e2a30f85507cdcb08951c33318984d9b023
Aleksey Shutiy <a.shutiy@gmail.com>
2020-04-06 21:04:38 +0300

    Create and configure application

---
+++ src/protected/config/database.php
@@ -0,0 +1,14 @@
+<?php
+
+// This is the database connection configuration.
+return array(
+	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	// uncomment the following lines to use a MySQL database
+
+	'connectionString' => 'mysql:host=db;dbname=logs',
+	'emulatePrepare' => true,
+	'username' => 'root',
+	'password' => getenv('DB_PASSWORD', true),
+	'charset' => 'utf8',
+
+);
